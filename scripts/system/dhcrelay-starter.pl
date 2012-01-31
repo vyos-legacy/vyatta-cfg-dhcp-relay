157849e8b0503deb40c1e9ca92b674c698610778
John Southworth <john.southworth@vyatta.com>
2012-01-31 10:56:22 -0800

    Add needed changes to dhcrelay-starter for operational mode restart

--- scripts/system/dhcrelay-starter.pl
+++ scripts/system/dhcrelay-starter.pl
@@ -6,24 +6,28 @@ use Vyatta::Config;
 use Vyatta::Interface;
 use Getopt::Long;
 
-my $init;
+my ($init, $op_mode);
 GetOptions(
-    "init=s"       => \$init
+    "init=s"       => \$init,
+    "op-mode!"    => \$op_mode
 );
+my $exists = ($op_mode ? 'existsOrig' : 'exists');
+my $returnValues = ($op_mode ? 'returnOrigValues' : 'returnValues');
+my $returnValue = ($op_mode ? 'returnOrigValue' : 'returnValue');
 
 my $vc     = new Vyatta::Config();
 my $vcRoot = new Vyatta::Config();
 my $cmd_args = "";
 
 $vc->setLevel('service dhcp-relay');
-if ( $vc->exists('.') ) {
+if ( $vc->$exists('.') ) {
 
-    my $port = $vc->returnValue("relay-options port");
+    my $port = $vc->$returnValue("relay-options port");
     if ( $port ne '' ) {
         $cmd_args .= " -p $port";
     }
 
-    my @interfaces = $vc->returnValues("interface");
+    my @interfaces = $vc->$returnValues("interface");
     foreach my $ifname (@interfaces) {
         my $intf = new Vyatta::Interface($ifname);
         die "DHCP relay configuration error."
@@ -32,27 +36,27 @@ if ( $vc->exists('.') ) {
 
         die
 "DHCP relay configuration error.  DHCP relay interface \"$ifname\" specified has not been configured.\n"
-          unless $vcRoot->exists( $intf->path() );
+          unless $vcRoot->$exists( $intf->path() );
 
         $cmd_args .= " -i " . $intf->name();
     }
 
-    my $count = $vc->returnValue("relay-options hop-count");
+    my $count = $vc->$returnValue("relay-options hop-count");
     if ( $count ne '' ) {
         $cmd_args .= " -c $count";
     }
 
-    my $length = $vc->returnValue("relay-options max-size");
+    my $length = $vc->$returnValue("relay-options max-size");
     if ( $length ne '' ) {
         $cmd_args .= " -A $length";
     }
 
-    my $rap = $vc->returnValue("relay-options relay-agents-packets");
+    my $rap = $vc->$returnValue("relay-options relay-agents-packets");
     if ( $rap ne '' ) {
         $cmd_args .= " -m $rap";
     }
 
-    my @servers = $vc->returnValues("server");
+    my @servers = $vc->$returnValues("server");
     if ( @servers == 0 ) {
         die
 "DHCP relay configuration error.  No DHCP relay server(s) configured.  At least one DHCP relay server required.\n";
